6b8a5fa41395f524b1a25eb98db83811516b15df
K4rlosReyes <carlosreyesml18@gmail.com>
2023-07-04 11:17:47 +0200

    dev: added timeseries keys

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ URL = "https://thingsboard.cloud"
 USERNAME = "pfernandez@mat.upv.es"
 PASSWORD = "123456"
 CUSTOMER_ID = "89ffe890-0e38-11ec-a4b0-6fb4a09a8a57"
-TIMESERIES = 'CO2'
+TIMESERIES = "Temperatura,CO2,Humedad,Dispositivos"
 DEVICE_LABELS = ["CUD-01", "CUD-02"]
 
 
@@ -16,12 +16,12 @@ DEVICE_LABELS = ["CUD-01", "CUD-02"]
 telgetter = TelemetryGetter()
 telgetter.set_url(url=URL)
 telgetter.set_credentials(username=USERNAME, password=PASSWORD)
-telgetter.set_customer_id( customer_id=CUSTOMER_ID )
+telgetter.set_customer_id(customer_id=CUSTOMER_ID)
 
 # Fetch telemetry data
-ldev = telgetter.fetch_telemetry( devices_label=DEVICE_LABELS, timeseries_key=TIMESERIES )
+ldev = telgetter.fetch_telemetry(devices_label=DEVICE_LABELS, timeseries_key=TIMESERIES)
 
 # Save Telemetry data
-saver = TelemetrySaver( database_address='database_test.db', table_name='telemetry' )
-saver.save_telemetry( ldev )
+saver = TelemetrySaver(database_address="database_test.db", table_name="telemetry")
+saver.save_telemetry(ldev)
 saver.close()
