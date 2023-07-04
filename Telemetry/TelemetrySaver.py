c594e972586c00ef0b3a15668802d2072c1f2cd5
K4rlosReyes <carlosreyesml18@gmail.com>
2023-07-04 18:57:17 +0200

    dev: added get_timestamp and update_timestamp

--- Telemetry/TelemetrySaver.py
+++ Telemetry/TelemetrySaver.py
@@ -1,8 +1,10 @@
 import sqlite3
+import datetime
 
 
 class TelemetrySaver(object):
     def __init__(self, database_address: str, table_name: str) -> None:
+        # Main Database Info
         self.database_address = database_address
         self.table_name = table_name
         self.conn = sqlite3.connect(self.database_address)
@@ -19,6 +21,20 @@ class TelemetrySaver(object):
         """
         )
 
+        # Timestamp Database Info
+        self.ts_database_address = "ts_" + database_address
+        self.ts_table_name = "ts_" + table_name
+        self.ts_conn = sqlite3.connect(self.ts_database_address)
+        self.ts_cursor = self.ts_conn.cursor()
+        self.ts_cursor.execute(
+            f"""
+			 CREATE TABLE IF NOT EXISTS {self.ts_table_name} (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TIMESTAMP
+			 )
+             """
+        )
+
     def save_telemetry(self, telemetry_data: dict) -> None:
         # Create a database connection
         for device_name, telemetry in telemetry_data.items():
@@ -33,6 +49,24 @@ class TelemetrySaver(object):
                         (device_name, time_stamp, timeseries_key, value),
                     )
         self.conn.commit()
+
+    def get_timestamp(self) -> str:
+        current_timestamp = datetime.datetime.now()
+        self.ts_cursor.execute(
+            f"SELECT COALESCE(timestamp, ?) FROM {self.ts_table_name}",
+            (current_timestamp,),
+        )
+        timestamp = self.ts_cursor.fetchone()
+        return timestamp
+
+    def update_timestamp(self):
+        current_timestamp = datetime.datetime.now()
+
+        # Update timestamp value
+        self.ts_cursor.execute(
+            f"UPDATE {self.ts_table_name} SET timestamp = ?", (current_timestamp,)
+        )
+        self.ts_connection.commit()
 
     def close(self) -> None:
         self.conn.close()
