ef334abc133837a946f0a341e2decc8898d0c298
K4rlosReyes <carlosreyesml18@gmail.com>
2023-07-06 16:36:50 +0200

    dev: created running script

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true; do
+  python main.py  # Reemplaza "main.py" por el nombre de tu archivo Python
+  sleep 1h       # Espera 1 hora antes de ejecutar el código nuevamente
+done
+
